7088f8a98aac62b564ead770791ea957bd6dbc49
WQY <171250017@smail.nju.edu.cn>
2020-10-10 14:11:42 +0800

    202010101411

--- src/Computation.py
+++ src/Computation.py
@@ -6,8 +6,13 @@ import datetime
 import pandas as pd
 import numpy as np
 
+#list1存放了与信息类一中属性最相似的信息类二中属性的下标，list2类似
+#weight1存放了权重
 list1=[]
+weight1=[]
 list2=[]
+weight2=[]
+#初始化属性之间的权重
 def init():
     pd1 = pd.read_excel('../dict/task2.xlsx', '信息类一')
     pd2 = pd.read_excel('../dict/task2.xlsx', '信息类二')
@@ -15,19 +20,21 @@ def init():
         max_similar=0
         max_j=0
         for j in range(0,len(pd2.iloc[0])):
-            d=iou_similarity(pd1.iloc[0][i],pd2.iloc[0][j],'single')
+            d=edit_dis_similarity(pd1.iloc[0][i],pd2.iloc[0][j])
             if(d>max_similar):
                 max_similar=d
                 max_j=j
+        weight1.append(max_similar)
         list1.append(max_j)
     for i in range(0,len(pd2.iloc[0])):
         max_similar=0
         max_j=0
         for j in range(0,len(pd1.iloc[0])):
-            d=iou_similarity(pd2.iloc[0][i],pd1.iloc[0][j],'redundancy')
+            d=edit_dis_similarity(pd2.iloc[0][i],pd1.iloc[0][j])
             if(d>max_similar):
                 max_similar=d
                 max_j=j
+        weight2.append(max_similar)
         list2.append(max_j)
 
 
@@ -35,19 +42,24 @@ def similarity_degree(obj1, obj2):
     init()
     sum=0
     for i in range(0,len(obj1)):
-        sum+=iou_similarity(obj1[i],obj2[list1[i]],'redundancy')
+        #排除空值
+        if(type(obj1[i])==type(0.3) or type(obj2[list1[i]])==type(0.3)):
+            sum+=0
+        else:
+            sum+=weight1[list1[i]]*iou_similarity(obj1[i],obj2[list1[i]],'redundancy')
     return sum
 
-# init()
-# pd1 = pd.read_excel('../dict/task2.xlsx', '信息类一')
-# pd2 = pd.read_excel('../dict/task2.xlsx', '信息类二')
-# max=0
-# maxi=0
-# for i in range(0,100):
-#     print(i)
-#     if(similarity_degree(pd1.iloc[1],pd2.iloc[i])>max):
-#         max=similarity_degree(pd1.iloc[1],pd2.iloc[i])
-#         maxi=i
+
+init()
+pd1 = pd.read_excel('../dict/task2.xlsx', '信息类一')
+pd2 = pd.read_excel('../dict/task2.xlsx', '信息类二')
+max=0
+maxi=0
+for i in range(0,100):
+    print("正在计算第"+str(i)+"个")
+    if(similarity_degree(pd1.iloc[1],pd2.iloc[i])>max):
+        max=similarity_degree(pd1.iloc[1],pd2.iloc[i])
+        maxi=i
 # print(similarity_degree(pd1.iloc[1],pd2.iloc[2]))
 # print(similarity_degree(pd1.iloc[1],pd2.iloc[80]))
-# print(maxi)
+print(maxi)
